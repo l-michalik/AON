8f102dd9162998a74da54dcdc67a0223d50a8419
Łukasz Michalik <l.michalik@mdbootstrap.com>
2025-06-22 16:44:10 +0200

    finish e-commerce-agent with bright data

--- e-commerce-ai-agent/app.py
+++ e-commerce-ai-agent/app.py
@@ -1,31 +1,32 @@
-import json
 import os
+import json
 import asyncio
+import logging
 
-from typing import List, Optional
-from langchain_openai import ChatOpenAI
+from typing import Optional
+
+from dotenv import load_dotenv
 from pydantic import BaseModel, Field
 from flask import Flask, render_template, redirect, url_for, flash, request
-from mcp import ClientSession, StdioServerParameters # type: ignore
-from mcp.client.stdio import stdio_client # type: ignore
-from langchain_mcp_adapters.tools import load_mcp_tools # type: ignore
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+
+from langchain_openai import ChatOpenAI
+from langchain_mcp_adapters.tools import load_mcp_tools
 from langgraph.prebuilt import create_react_agent
-from dotenv import load_dotenv
+
+logging.getLogger("root").setLevel(logging.ERROR)
 
 load_dotenv()
-
-model = ChatOpenAI(
-    model_name="gpt-4o-mini",
-    api_key=os.getenv("OPENAI_API_KEY"),
-)
+model = ChatOpenAI(model='gpt-4o')
 
 server_params = StdioServerParameters(
-    command="npx",
+    command='npx',
     args=['@brightdata/mcp'],
-    env={
-        'API_TOKEN': os.getenv("BRIGHT_DATA_API_KEY"),
-        'BROWSER_AUTH': os.getenv("BROWSER_AUTH"),
-        'WEB_UNLOCKER_ZONE': os.getenv("WEB_UNLOCKER_ZONE"),
+    env = {
+        'API_TOKEN': os.getenv('BRIGHT_DATA_API_KEY'),
+        'BROWSER_AUTH': os.getenv('BROWSER_AUTH'),
+        'WEB_UNLOCKER_ZONE': os.getenv('WEB_UNLOCKER_ZONE')
     }
 )
 
@@ -37,55 +38,50 @@ SYSTEM_PROMPT = (
 PLATFORMS = ['Amazon', 'Shopify', 'Temu']
 
 class Hit(BaseModel):
-    url: str = Field(..., description="The URL of the product")
-    title: str = Field(..., description="The title of the product")
-    rating: str = Field(..., description="The rating of the product")
+    url: str = Field(..., description='The URL of the product that was found')
+    title: str = Field(..., description='The title of the product that was found')
+    rating: str = Field(..., description='The rating of the product (stars, number of ratings given etc.)')
+
 
 class PlatformBlock(BaseModel):
-    platform: str = Field(..., description="The platform to search on")
-    results: List[Hit] = Field(..., description="The list of results found on the platform")
+    platform: str = Field(..., description='Name of the platform')
+    results: list[Hit] = Field(..., description='List of results for this platform')
+
 
 class ProductSearchResponse(BaseModel):
-    platforms: List[PlatformBlock] = Field(..., description="The list of platforms and their results")
-    
-    
+    platforms: list[PlatformBlock] = Field(..., description='Aggregated list of all results grouped by platform')
+
+
+
 app = Flask(__name__)
-app.secret_key = 'msk'
+app.secret_key = 'mysecretkey-not-for-prod'
+
 
 async def run_agent(query, platforms):
     async with stdio_client(server_params) as (read, write):
         async with ClientSession(read, write) as sess:
-            await sess.init()
-            
+            await sess.initialize()
+
             tools = await load_mcp_tools(sess)
-            
-            agent = create_react_agent(
-                model=model,
-                tools=tools,
-                response_format=ProductSearchResponse,
-            )
-            
-            prompt = f"{query}\n\nPlatforms: {', '.join(platforms)}"
-            
+
+            agent = create_react_agent(model, tools, response_format=ProductSearchResponse)
+
+            prompt = f'{query}\n\nPlatforms: {",".join(platforms)}'
+
             result = await agent.ainvoke(
                 {
-                    "mesages": [
-                        {
-                            "role": "system",
-                            "content": SYSTEM_PROMPT
-                        },
-                        {
-                            "role": "user",
-                            "content": prompt
-                        }
+                    'messages': [
+                        {'role': 'system', 'content': SYSTEM_PROMPT},
+                        {'role': 'user', 'content': prompt}
                     ]
                 }
             )
-            
+
             structured = result['structured_response']
-            
+
             return structured.model_dump()
-        
+
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
@@ -97,7 +93,7 @@ def index():
         if not platforms:
             flash("Select at least one platform.", "danger")
             return redirect(url_for("index"))
-        
+
         try:
             response_json = asyncio.run(run_agent(query, platforms))
         except Exception as exc:
@@ -119,6 +115,7 @@ def index():
         selected=[],
         response=None,
     )
-    
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
